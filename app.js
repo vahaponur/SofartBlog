09e8167608c53b86cbad1cbeb31131a74d460db8
vahaponur <vahaponuryildirim@gmail.com>
2021-10-07 02:03:45 +0300

    BSDFAS

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ const postSchema = new mongoose.Schema({
     imageName:{
         type:String,
         required:true
+    },
+    type:{
+        type:String
     }
 });
 const Post = mongoose.model('Post',postSchema);
@@ -77,32 +80,65 @@ app.route('/').get((req,res)=>{
 
 //Yeni Post Girişi
 app.route('/newentry').get((req,res)=>{
-    res.render('newentry',{pageTitle:'Sofart - Post Girişi'});
+    
+    Person.find({},(err,results)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            if(results){
+                res.render('newentry',{pageTitle:'Sofart - Post Girişi',personlist:results});
+            }
+        }
+
+    })
+    
 }).post((req,res)=>{
     if(req.files){
         const title = req.body.postTitle;
         const content = req.body.postContent;
         const summary = req.body.postSummary;
         const file = req.files.postPicture;
+        let personone;
+        let persontwo;
+        Person.findById(req.body.personOne,(err,person)=>{
+            if(err)
+                res.render(err);
+            else{
+                personone = person
+            }
+        });
+        Person.findById(req.body.personTwo,(err,person)=>{
+            if(err)
+                res.render(err);
+            else{
+                persontwo = person
+            }
+        });
+        
+
         let filename = file.name;
-        let path = __dirname + './public/img/'+filename;
+        let path = __dirname + '/public/img/'+filename;
         let filenumber = 0;
         while (fs.existsSync(path)) {
             filename = filenumber.toString() + filename;
             filenumber++;
-            path = __dirname + './public/img/'+filename;
+            path = __dirname + '/public/img/'+filename;
         };
         const post = new Post({
             title:title,
             content:content,
             summary:summary,
-            imageName:'/img/'+filename
+            imageName:'/img/'+filename,
+            person1:personone,
+            person2:persontwo
+            
         });
         file.mv('./public/img/'+filename);
         post.save();
         
         
-        res.redirect('/');
+        res.redirect('/newentry');
     }
     else{
         res.send('resim olmadan yollanmaz');
@@ -113,6 +149,7 @@ app.route('/addperson').get((req,res)=>{
     res.render('addperson',{pageTitle:'Kişi Ekle'});
 }).post((req,res)=>{
     if(req.files){
+        console.log(__dirname);
         const fname = req.body.fname;
         const lname = req.body.lname;
         const rutbe = req.body.rutbe;
@@ -123,30 +160,34 @@ app.route('/addperson').get((req,res)=>{
         const pinterest = req.body.pinterest;
         const personalWebsite = req.body.personalwebsite;
         let filename = file.name;
-        let path = __dirname + './public/img/personimg/'+filename;
+        let path = __dirname + '/public/img/personimg/'+filename;
         let filenumber = 0;
         while (fs.existsSync(path)) {
             filename = filenumber.toString() + filename;
             filenumber++;
-            path = __dirname + './public/img/personimg/'+filename;
+            path = __dirname + '/public/img/personimg/'+filename;
         };
-        const person = new Person({
-            firstName:fname,
-            lastName:lname,
-            rutbe:rutbe,
-            picture:'/img/personimg/'+filename,
-            linkedin:linkedin,
-            email:email,
-            behance:behance,
-            pinterest:pinterest,
-            personalWebsite:personalWebsite
-
-        });
-        file.mv('./public/img/personimg/'+filename);
-        person.save();
-        
-        
-        res.redirect('/');
+        if(!fs.existsSync(path)){
+            
+            const person = new Person({
+                firstName:fname,
+                lastName:lname,
+                rutbe:rutbe,
+                picture:'/img/personimg/'+filename,
+                linkedin:linkedin,
+                email:email,
+                behance:behance,
+                pinterest:pinterest,
+                personalWebsite:personalWebsite
+    
+            });
+            file.mv('./public/img/personimg/'+filename);
+            person.save();
+            
+            
+            res.redirect('/addperson');
+        }
+      
     }
     else{
         res.send('resim olmadan yollanmaz');
