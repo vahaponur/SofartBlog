7f7f409ce07c36c45fb850b96185286bc7b9a62d
vahaponur <vahaponuryildirim@gmail.com>
2021-10-07 05:31:37 +0300

    v 0.02
    Yazar grafiker seçilebiliyor idler alınıyor inş

--- app.js
+++ app.js
@@ -1,155 +1,170 @@
+const { assert } = require('console');
 const express = require('express');
 const upload = require('express-fileupload');
 const fs = require('fs');
 const mongoose = require('mongoose');
+
 const app = express();
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 app.use(upload());
 
 mongoose.connect('mongodb://localhost:27017/sofartDB');
 const personSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
+    firstName: {
+        type: String,
+        required: true
     },
-    lastName:{
-        type:String,
-        required:true
+    lastName: {
+        type: String,
+        required: true
     },
-    rutbe:{
-        type:String,
-        required:true
+    rutbe: {
+        type: String,
+        required: true
     },
-    picture:{
-        type:String,
-        required:true
+    picture: {
+        type: String,
+        required: true
     },
-    linkedin:{
-        type:String
+    linkedin: {
+        type: String
     },
-    email:{
-        type:String
+    email: {
+        type: String
     },
-     behance:{
-        type:String
+    behance: {
+        type: String
     },
-    pinterest:{
-        type:String
+    pinterest: {
+        type: String
     },
-    personalWebsite:{
-        type:String
+    personalWebsite: {
+        type: String
     }
 
 });
-const Person = mongoose.model('person',personSchema);
+const Person = mongoose.model('person', personSchema);
 const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    summary:{
-        type:String,
-        required:true
+    summary: {
+        type: String,
+        required: true
     },
-    content:{
-        type:String,
-        required:true
+    content: {
+        type: String,
+        required: true
     },
-   
-    person1:personSchema,
-    person2:personSchema,
-    imageName:{
-        type:String,
-        required:true
+
+    person1: mongoose.type.ObjectId,
+    person2: mongoose.type.ObjectId,
+    imageName: {
+        type: String,
+        required: true
     },
-    type:{
-        type:String
+    tip: {
+        type: String
     }
 });
-const Post = mongoose.model('Post',postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 //Anasayfa Girişi
-app.route('/').get((req,res)=>{
-    Post.find({}).sort({'_id':-1}).limit(3).then(results=>{
-        res.render('index',{pageTitle:'SofART Sanat ve Kültür Dergisi',featuredPosts:results});
+app.route('/').get((req, res) => {
+    Post.find({}).sort({ '_id': -1 }).limit(3).then(results => {
+        res.render('index', { pageTitle: 'SofART Sanat ve Kültür Dergisi', featuredPosts: results });
     })
-    
+
 })
 
 //Yeni Post Girişi
-app.route('/newentry').get((req,res)=>{
-    
-    Person.find({},(err,results)=>{
-        if(err){
+app.route('/newentry').get((req, res) => {
+
+    Person.find({}, (err, results) => {
+        if (err) {
             res.send(err);
         }
-        else{
-            if(results){
-                res.render('newentry',{pageTitle:'Sofart - Post Girişi',personlist:results});
+        else {
+            if (results) {
+                res.render('newentry', { pageTitle: 'Sofart - Post Girişi', personlist: results });
             }
         }
 
     })
-    
-}).post((req,res)=>{
-    if(req.files){
-        const title = req.body.postTitle;
-        const content = req.body.postContent;
-        const summary = req.body.postSummary;
-        const file = req.files.postPicture;
-        let personone;
-        let persontwo;
-        console.log(req.body.personone);
-        Person.findById(req.body.personone,(err,person)=>{
-            if(err)
-                res.render(err);
-            else{
-                personone = person
-            }
-        });
-        Person.findById(req.body.persontwo,(err,person)=>{
-            if(err)
-                res.render(err);
-            else{
-                persontwo = person
-            }
-        });
-        
 
-        let filename = file.name;
-        let path = __dirname + '/public/img/'+filename;
-        let filenumber = 0;
-        while (fs.existsSync(path)) {
-            filename = filenumber.toString() + filename;
-            filenumber++;
-            path = __dirname + '/public/img/'+filename;
-        };
-        const post = new Post({
-            title:title,
-            content:content,
-            summary:summary,
-            imageName:'/img/'+filename,
-            person1:personone,
-            person2:persontwo
-            
-        });
-        file.mv('./public/img/'+filename);
-        post.save();
-        
-        
-        res.redirect('/newentry');
-    }
-    else{
-        res.send('resim olmadan yollanmaz');
-    }
+}).post((req, res) => {
     
+    
+    
+    // const query1 = async()=>{
+        
+    //     await Person.findById(mongoose.Types.ObjectId(req.body.personone.toString()),(err,res)=>{
+    //         personone = res;
+    //     });
+    // };
+    // const query2 =  async()=>{
+        
+    //     await Person.findById(mongoose.Types.ObjectId(req.body.personone.toString()),(err,res)=>{
+    //         persontwo = res;
+    //     });
+        
+    // };
+    let promises = [
+        Person.findById(mongoose.Types.ObjectId(req.body.personone.toString())).exec(),
+        Person.findById(mongoose.Types.ObjectId(req.body.persontwo.toString())).exec(),
+    ];
+ 
+    Promise.all(promises).then((data)=>{
+        
+        let personone = data[0]._id;
+        let persontwo = data[1]._id;
+       
+        if (req.files) {
+            const title = req.body.postTitle;
+            const content = req.body.postContent;
+            const summary = req.body.postSummary;
+            const file = req.files.postPicture;
+            console.log(persontwo);
+            let filename = file.name;
+            let path = __dirname + '/public/img/' + filename;
+            let filenumber = 0;
+            while (fs.existsSync(path)) {
+                filename = filenumber.toString() + filename;
+                filenumber++;
+                path = __dirname + '/public/img/' + filename;
+            };
+            const post = new Post({
+                title: title,
+                content: content,
+                summary: summary,
+                imageName: '/img/' + filename,
+                person1: personone,
+                person2: persontwo
+    
+            });
+            
+            file.mv('./public/img/' + filename);
+            post.save().then(()=>{
+                res.redirect('/newentry');
+            })
+    
+            
+        }
+        else {
+            res.send('resim olmadan yollanmaz');
+        }
+    });
+
+  
+
 })
-app.route('/addperson').get((req,res)=>{
-    res.render('addperson',{pageTitle:'Kişi Ekle'});
-}).post((req,res)=>{
-    if(req.files){
+app.route('/addperson').get((req, res) => {
+    res.render('addperson', { pageTitle: 'Kişi Ekle' });
+}).post((req, res) => {
+    if (req.files) {
         console.log(__dirname);
         const fname = req.body.fname;
         const lname = req.body.lname;
@@ -161,40 +176,40 @@ app.route('/addperson').get((req,res)=>{
         const pinterest = req.body.pinterest;
         const personalWebsite = req.body.personalwebsite;
         let filename = file.name;
-        let path = __dirname + '/public/img/personimg/'+filename;
+        let path = __dirname + '/public/img/personimg/' + filename;
         let filenumber = 0;
         while (fs.existsSync(path)) {
             filename = filenumber.toString() + filename;
             filenumber++;
-            path = __dirname + '/public/img/personimg/'+filename;
+            path = __dirname + '/public/img/personimg/' + filename;
         };
-        if(!fs.existsSync(path)){
-            
+        if (!fs.existsSync(path)) {
+
             const person = new Person({
-                firstName:fname,
-                lastName:lname,
-                rutbe:rutbe,
-                picture:'/img/personimg/'+filename,
-                linkedin:linkedin,
-                email:email,
-                behance:behance,
-                pinterest:pinterest,
-                personalWebsite:personalWebsite
-    
+                firstName: fname,
+                lastName: lname,
+                rutbe: rutbe,
+                picture: '/img/personimg/' + filename,
+                linkedin: linkedin,
+                email: email,
+                behance: behance,
+                pinterest: pinterest,
+                personalWebsite: personalWebsite
+
             });
-            file.mv('./public/img/personimg/'+filename);
+            file.mv('./public/img/personimg/' + filename);
             person.save();
-            
-            
+
+
             res.redirect('/addperson');
         }
-      
+
     }
-    else{
+    else {
         res.send('resim olmadan yollanmaz');
     }
 })
 const PORT = 3169;
-app.listen(3169,()=>{
-    console.log('Server up and running on Port: '+PORT.toLocaleString());
+app.listen(3169, () => {
+    console.log('Server up and running on Port: ' + PORT.toLocaleString());
 })
